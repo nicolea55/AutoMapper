87fcf2d422870a2b93bbcd76e1ad8db3029161ad
Nicolea B <nicu@inbox.ru>
2024-06-07 10:50:08 +0300

    Corrected net version for prev version lib in PreBuild.sh

--- src/AutoMapper/ApiCompat/PreBuild.sh
+++ src/AutoMapper/ApiCompat/PreBuild.sh
@@ -12,4 +12,4 @@ oldVersion="$oldVersion.0.0"
 echo $oldVersion
 rm -rf ../LastMajorVersionBinary
 curl https://globalcdn.nuget.org/packages/automapper.$oldVersion.nupkg --create-dirs -o ../LastMajorVersionBinary/automapper.$oldVersion.nupkg
-unzip -j ../LastMajorVersionBinary/automapper.$oldVersion.nupkg lib/netstandard2.1/AutoMapper.dll -d ../LastMajorVersionBinary
+unzip -j ../LastMajorVersionBinary/automapper.$oldVersion.nupkg lib/net6.0/AutoMapper.dll -d ../LastMajorVersionBinary
